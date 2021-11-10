0d486acb564ab0b7a519be269b36d4020ec275eb
Marko Engelman <engelman.marko@gmail.com>
2021-11-10 19:45:26 +0100

    Initial Commit

---
+++ Observable/Observable.h
@@ -0,0 +1,18 @@
+//
+//  Observable.h
+//  Observable
+//
+//  Created by Marko Engelman on 10/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Observable.
+FOUNDATION_EXPORT double ObservableVersionNumber;
+
+//! Project version string for Observable.
+FOUNDATION_EXPORT const unsigned char ObservableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Observable/PublicHeader.h>
+
+
